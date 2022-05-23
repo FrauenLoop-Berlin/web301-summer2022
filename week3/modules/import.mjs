debb7ec19105311fcc9a1e301538a87350dbf384
Paloma Oliveira <paloma.oliveira@saucelabs.com>
2022-05-23 21:12:03 +0200

    Add css styles and variables

--- week3/modules/import.mjs
+++ week3/modules/import.mjs
@@ -2,7 +2,7 @@ import * as printing from './export.mjs';
 import { printName, printNumber } from './export.mjs';
 
 printing.printName( 'Paloma' );
-printing.printNameVariable( 'Paloma' );
+printing.printNameVariable( 'Paloma Variable' );
 
 printName('George');
 printNumber(123);
