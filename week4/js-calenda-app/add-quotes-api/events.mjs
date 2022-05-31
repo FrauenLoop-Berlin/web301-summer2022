0edcdb614aa1c92402cca984e7f2c1809a635587
discombobulateme <paloma.oliveira@saucelabs.com>
2022-05-31 13:05:28 +0200

    Create add-quote folder

---
+++ week4/js-calenda-app/add-quotes-api/events.mjs
@@ -0,0 +1,25 @@
+import { closeModal } from './modal.mjs';
+
+function saveEvent() {
+  if (eventTitleInput.value) {
+    eventTitleInput.classList.remove('error');
+
+    events.push({
+      date: clicked,
+      title: eventTitleInput.value,
+    });
+
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
+  } else {
+    eventTitleInput.classList.add('error');
+  }
+}
+
+function deleteEvent() {
+  events = events.filter(e => e.date !== clicked);
+  localStorage.setItem('events', JSON.stringify(events));
+  closeModal();
+}
+
+export { saveEvent, deleteEvent };
